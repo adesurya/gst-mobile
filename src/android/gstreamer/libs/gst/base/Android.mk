7138e3cdaf9531f77283ae9bd8dc83916722a282
peterxu <uskee521@gmail.com>
2012-09-05 14:43:57 +0000

    glib and gstreamer for android

---
+++ src/android/gstreamer/libs/gst/base/Android.mk
@@ -0,0 +1,45 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+GST_MAJORMINOR:= 0.10
+
+LOCAL_SRC_FILES:= \
+    gstadapter.c		\
+    gstbasesink.c		\
+    gstbasesrc.c		\
+    gstbasetransform.c	\
+    gstcollectpads.c	\
+    gstpushsrc.c		\
+    gsttypefindhelper.c	\
+    gstdataqueue.c
+
+LOCAL_STATIC_LIBRARIES := \
+    gstreamer-0.10       \
+    glib-2.0             \
+    gthread-2.0          \
+    gmodule-2.0          \
+    gobject-2.0
+
+LOCAL_MODULE:= gstbase-$(GST_MAJORMINOR)
+
+#LOCAL_TOP_PATH := $(LOCAL_PATH)/../../../..
+
+LOCAL_CFLAGS := 			\
+    -I$(LOCAL_PATH)   			\
+    -I$(GSTREAMER_TOP)       		\
+    -I$(GSTREAMER_TOP)/android   	\
+    -I$(GSTREAMER_TOP)/gst		\
+    -I$(GSTREAMER_TOP)/gst/android	\
+    -I$(GSTREAMER_TOP)/libs 		\
+    -I$(GSTREAMER_TOP)/../glib  	\
+    -I$(GSTREAMER_TOP)/../glib/android 	\
+    -I$(GSTREAMER_TOP)/../glib/glib  	\
+    -I$(GSTREAMER_TOP)/../glib/gmodule 	\
+    -I$(GSTREAMER_TOP)/../glib/gobject 	\
+    -I$(GSTREAMER_TOP)/../glib/gthread
+
+LOCAL_CFLAGS += \
+    -DHAVE_CONFIG_H			
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ src/android/gstreamer/libs/gst/controller/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+GST_MAJORMINOR:= 0.10
+
+LOCAL_SRC_FILES:= \
+    lib.c \
+    gstcontroller.c \
+    gstinterpolation.c \
+    gsthelper.c \
+    gstcontrolsource.c \
+    gstinterpolationcontrolsource.c \
+    gstlfocontrolsource.c
+
+LOCAL_STATIC_LIBRARIES := \
+    gstreamer-0.10       \
+    glib-2.0             \
+    gthread-2.0          \
+    gmodule-2.0          \
+    gobject-2.0
+
+LOCAL_MODULE:= gstcontroller-$(GST_MAJORMINOR)
+
+LOCAL_CFLAGS := 				\
+	-I$(GSTREAMER_TOP) 			\
+	-I$(GSTREAMER_TOP)/android 		\
+	-I$(GSTREAMER_TOP)/libs			\
+	-I$(GSTREAMER_TOP)/gst			\
+	-I$(GSTREAMER_TOP)/gst/android		\
+	-I$(GSTREAMER_TOP)/../glib		\
+	-I$(GSTREAMER_TOP)/../glib/android	\
+	-I$(GSTREAMER_TOP)/../glib/glib		\
+	-I$(GSTREAMER_TOP)/../glib/gmodule	\
+	-I$(GSTREAMER_TOP)/../glib/gobject  	\
+	-I$(GSTREAMER_TOP)/../glib/gthread
+
+LOCAL_CFLAGS += \
+	-DHAVE_CONFIG_H			
+
+include $(BUILD_STATIC_LIBRARY)
