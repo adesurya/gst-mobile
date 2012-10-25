0fa6548b5b8d67c06f13b0cf56077f25165541cd
uskee <uskee521@gmail.com>
2012-10-25 20:02:33 +0800

    add gst-plugins-bad for android

---
+++ src/android/gst-plugins-bad/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+GSTREAMER_TOP := $(LOCAL_PATH)
+
+include $(CLEAR_VARS)
+
+include $(GSTREAMER_TOP)/android/h264parse.mk
+include $(GSTREAMER_TOP)/android/sdpelem.mk
+include $(GSTREAMER_TOP)/android/metadata.mk
+include $(GSTREAMER_TOP)/android/qtmux.mk
+include $(GSTREAMER_TOP)/android/aacparse.mk
+include $(GSTREAMER_TOP)/android/amrparse.mk
+include $(GSTREAMER_TOP)/sys/audioflingersink/Android.mk
