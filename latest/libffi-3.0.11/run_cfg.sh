15872e79331b547ae66170e98767091aaf3e2733
Yongze Xu <yongze@yongze-VirtualBox.(none)>
2014-01-07 15:57:30 +0800

    add run_cfg.sh for libffi

---
+++ latest/libffi-3.0.11/run_cfg.sh
@@ -0,0 +1,26 @@
+ROOT=/opt/zdisk/zerox/android/android-ndk-r9
+
+CCROOT=$ROOT/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86
+SYSROOT=$ROOT/platforms/android-14/arch-arm/
+HOST=arm-linux-androideabi
+CACHE_FILE=./arm-linux.cache
+
+
+#export AR="$CCROOT/bin/$HOST-ar --sysroot=$SYSROOT"
+export CC="$CCROOT/bin/$HOST-gcc --sysroot=$SYSROOT"
+export CXX="$CCROOT/bin/$HOST-g++ --sysroot=$SYSROOT"
+export STRIP="$CCROOT/bin/$HOST-strip --sysroot=$SYSROOT"
+#export RANLIB="$CCROOT/bin/$HOST-ranlib --sysroot=$SYSROOT"
+export CFLAGS=""
+export LDFLAGS=""
+
+
+
+##
+## start configure
+mkdir -p oldbld
+cd oldbld
+PREFIX=`pwd`/libffi
+mkdir -p $PREFIX
+
+../configure --prefix=$PREFIX --host=arm 
