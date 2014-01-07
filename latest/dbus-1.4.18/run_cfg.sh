0928fe5e38ef1eb821ebc09170db5ca0625b4cad
Yongze Xu <yongze@yongze-VirtualBox.(none)>
2014-01-07 18:34:43 +0800

    add expat and dbus

---
+++ latest/dbus-1.4.18/run_cfg.sh
@@ -0,0 +1,28 @@
+ROOT=/opt/zdisk/zerox/android/android-ndk-r9
+
+CCROOT=$ROOT/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86
+SYSROOT=$ROOT/platforms/android-14/arch-arm/
+HOST=arm-linux-androideabi
+CACHE_FILE=./arm-linux.cache
+
+
+#export AR="$CCROOT/bin/$HOST-ar --sysroot=$SYSROOT"
+export CC="$CCROOT/bin/$HOST-gcc --sysroot=$SYSROOT"
+export CXX="$CCROOT/bin/$HOST-g++ --sysroot=$SYSROOT"
+export STRIP="$CCROOT/bin/$HOST-strip --sysroot=$SYSROOT"
+#export RANLIB="$CCROOT/bin/$HOST-ranlib --sysroot=$SYSROOT"
+export CFLAGS=""
+export LDFLAGS=""
+
+
+
+##
+## start configure
+mkdir -p oldbld
+cd oldbld
+PREFIX=`pwd`/libdbus
+mkdir -p $PREFIX
+
+../configure --prefix=$PREFIX --host=arm 
+
+make; make install
