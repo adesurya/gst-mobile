14e3456530f4a209fada4085729a97c34550c458
PeterXu <uskee521@gmail.com>
2014-01-08 15:48:02 +0800

    update

--- latest/groot/run_all.sh
+++ latest/groot/run_all.sh
@@ -2,25 +2,30 @@
 
 PWD=`pwd`/..
 
+####################
 cd $PWD/libffi-3.0.11
 sh run_cfg.sh
 
 cd $PWD/glib2.0-2.32.1  
 sh run_cfg.sh
 
-cd $PWD/gstreamer-1.2.2  
+
+####################
+gst_ver=1.2.2
+
+cd $PWD/gstreamer-$gst_ver
 sh run_cfg.sh
 
-cd $PWD/gst-plugins-base-1.2.2  
+cd $PWD/gst-plugins-base-$gst_ver
 sh run_cfg.sh
 
-cd $PWD/gst-plugins-good-1.2.2  
+cd $PWD/gst-plugins-good-$gst_ver
 sh run_cfg.sh
 
-cd $PWD/gst-plugins-ugly-1.2.2  
+cd $PWD/gst-plugins-ugly-$gst_ver
 sh run_cfg.sh
 
-cd $PWD/gst-plugins-bad-1.2.2  
+cd $PWD/gst-plugins-bad-$gst_ver
 sh run_cfg.sh
 
 exit 0
