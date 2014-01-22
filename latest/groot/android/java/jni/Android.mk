38dfc7010655407ef118754e20d39447abac4bac
PeterXu <uskee521@gmail.com>
2014-01-22 19:14:57 +0800

    update

--- latest/groot/android/java/jni/Android.mk
+++ latest/groot/android/java/jni/Android.mk
@@ -14,6 +14,7 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS += -DHAS_ATOMICS=1 -DHAVE_PTHREAD_H 
 LOCAL_CFLAGS += -I../../../include/gstreamer-1.0 -I../../../include/glib-2.0 -I../../../lib/glib-2.0/include
 LOCAL_LDLIBS += -llog
-LOCAL_LDFLAGS += -L../../../lib -lgstreamer-1.0 -lgobject-2.0 -lglib-2.0
+LOCAL_LDFLAGS += -L../../../lib -lgstreamer-1.0 -lgobject-2.0 -lgmodule-2.0 -lglib-2.0
+LOCAL_LDFLAGS += -lffi -landroid_support
 
 include $(BUILD_SHARED_LIBRARY)
